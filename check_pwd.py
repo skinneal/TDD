a128886e9e089a183569e679e1d97eec8fe83d5c
Allison Skinner <skinneal@oregonstate.edu>
2020-07-27 16:04:18 -0700

    Wrote if statement for the fourth test case. If the string contains at least one lowercase letter, return true

--- check_pwd.py
+++ check_pwd.py
@@ -3,13 +3,17 @@
 # Date: 7/27/2020
 
 def check_pwd(pwd):
-    if pwd == '':
+    if pwd == '' :
         return False
 
-    if len(pwd) <= 8 :
+    if len(pwd) < 8 :
         return False
 
-    if len(pwd) >= 20 :
+    if len(pwd) > 20 :
         return False
+
+    for char in pwd:
+        if char.islower():
+            return True
     return True
 
